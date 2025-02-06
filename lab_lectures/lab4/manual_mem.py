09899b4215befaf7011fe562e74105ee1a81a9bf
Michaelgathara <mikegtrm@gmail.com>
2025-02-06 01:22:49 -0600

    feat: added manual mem managed python

--- lab_lectures/lab4/manual_mem.py
+++ lab_lectures/lab4/manual_mem.py
@@ -23,11 +23,16 @@ def create_objects():
     return objects
 
 def main():
-    print("Garbage collector enabled:", gc.isenabled())
+    print("Garbage collector enabled:", gc.isenabled())    
     print("Creating objects...")
     
     # Create objects and store them
     objects = create_objects()
+    
+    # Let's see the amount of memory each object takes
+    print("\nObjects memory usage")
+    for obj_name, obj in objects.items():
+        print(f"{obj_name}: {format(obj.data.__sizeof__(), ',')} Bytes")
     
     # Let's see the objects we've created
     print("\nBefore manual cleanup:")
@@ -40,6 +45,11 @@ def main():
     
     # Force garbage collection despite it being disabled
     gc.collect()
+    
+    print("\nRemaining objects memory usage:")
+    for obj_name, obj in objects.items():
+        print(f"{obj_name}: {format(obj.data.__sizeof__(), ',')} Bytes")
+        
     
     print("\nAfter manual cleanup:")
     print("Remaining objects:", list(objects.keys()))
@@ -61,3 +71,43 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+"""
+Sample output incase you dont want to run this program:
+
+Garbage collector enabled: False
+Creating objects...
+Created object_0
+Created object_1
+Created object_2
+Created object_3
+Created object_4
+
+Objects memory usage
+object_0: 8,000,040 Bytes
+object_1: 8,000,040 Bytes
+object_2: 8,000,040 Bytes
+object_3: 8,000,040 Bytes
+object_4: 8,000,040 Bytes
+
+Before manual cleanup:
+Current objects: ['object_0', 'object_1', 'object_2', 'object_3', 'object_4']
+
+Performing manual cleanup...
+
+Remaining objects memory usage:
+object_0: 8,000,040 Bytes
+object_2: 8,000,040 Bytes
+object_4: 8,000,040 Bytes
+
+After manual cleanup:
+Remaining objects: ['object_0', 'object_2', 'object_4']
+
+After manual cleanup two:
+Remaining objects: []
+
+Garbage collector re-enabled: True
+
+"""
